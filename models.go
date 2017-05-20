ff6d7707a8ee478316b4e958e57b489bcba296b1
Hung Nguyen <nphung@elarvee.xyz>
2017-05-20 22:25:21 +0700

    Mail: Add recipient

--- models.go
+++ models.go
@@ -5,6 +5,7 @@ import "time"
 type Mail struct {
 	From      string    `gorethink:"from"`
 	To        string    `gorethink:"to"`
+	Recipient string    `schema:"recipient" gorethink:"recipient"`
 	Subject   string    `gorethink:"subject"`
 	BodyPlain string    `schema:"body-plain" gorethink:"body-plain"`
 	BodyHtml  string    `schema:"body-html" gorethink:"body-html"`
