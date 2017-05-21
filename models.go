b37bc77b9fcd9b5d390fa64c9a173462b76afe4e
Hung Nguyen <nphung@elarvee.xyz>
2017-05-21 16:26:41 +0700

    Mail: Rearrange tags

--- models.go
+++ models.go
@@ -5,10 +5,10 @@ import "time"
 type Mail struct {
 	From      string    `gorethink:"from"`
 	To        string    `gorethink:"to"`
-	Recipient string    `schema:"recipient" gorethink:"recipient"`
+	Recipient string    `gorethink:"recipient" schema:"recipient"`
 	Subject   string    `gorethink:"subject"`
-	BodyPlain string    `schema:"body-plain" gorethink:"body-plain"`
-	BodyHtml  string    `schema:"body-html" gorethink:"body-html"`
+	BodyPlain string    `gorethink:"body-plain" schema:"body-plain"`
+	BodyHtml  string    `gorethink:"body-html" schema:"body-html"`
 	Date      time.Time `gorethink:"date"`
 }
 
