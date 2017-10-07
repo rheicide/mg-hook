4922724a0a0d7b79532ce856a374dc6642fd79aa
Hung Nguyen <nphung@elarvee.xyz>
2017-10-07 21:25:52 +0700

    Handle another time format

--- handlers.go
+++ handlers.go
@@ -56,7 +56,10 @@ func init() {
 			if err != nil {
 				date, err = time.Parse("02 Jan 2006 15:04:05 -0700", value)
 				if err != nil {
-					log.Fatalln(err)
+					date, err = time.Parse("2 Jan 2006 15:04:05 -0700", value)
+					if err != nil {
+						log.Fatalln(err)
+					}
 				}
 			}
 		}
